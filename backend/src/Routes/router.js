11dd11989fadbfa8155cc15ebc2397286e3cfd34
leh <leticiazanellattodeoliveira@gmail.com>
2024-12-28 10:49:46 -0300

    crud evento

--- backend/src/Routes/router.js
+++ backend/src/Routes/router.js
@@ -3,5 +3,11 @@ const EventoController = require('../Controller/EventoController')
 
 const routes = Router()
 
+//Eventos CRUD
 routes.post('/evento', EventoController.create)
+routes.get('/eventos', EventoController.index)
+routes.put('/evento/:id', EventoController.update)
+routes.delete('/evento/"id', EventoController.delete)
 
+
+module.exports = routes
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,16 @@
 const express = require('express');
-const pool = require('./conexao');
-
+const router = require('./Routes/router')
 const app = express();
 
 // Middleware para JSON
 app.use(express.json());
+app.use(router)
 
 // Rota de teste
 app.get('/', (req, res) => {
   res.send('API está funcionando!');
 });
 
-// Rota para listar dados de exemplo
-app.get('/users', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM users');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Erro no servidor');
-  }
-});
 
 // Porta do servidor
 const PORT = 5000;
